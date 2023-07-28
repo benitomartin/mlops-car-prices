84df53702a590e45311322cda4b8b5791905d649
bmartinc80 <bmartinc80@gmail.com>
2023-07-28 22:00:52 +0200

    pylint4

---
+++ terraform/modules/S3/main.tf
@@ -0,0 +1,11 @@
+# This Bucket is not the same as the state bucket
+# It will be created by terraform
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket = var.bucket_name
+  acl    = "private"
+  force_destroy = true
+}
+
+output "name" {
+  value = aws_s3_bucket.s3_bucket.bucket
+}
