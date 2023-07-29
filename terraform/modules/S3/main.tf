9749447c8117a136153492d54e69669ab98b4a1b
bmartinc80 <bmartinc80@gmail.com>
2023-07-29 12:27:47 +0200

    Rename folder S3 to s3

--- terraform/modules/S3/main.tf
+++ terraform/modules/S3/main.tf
@@ -1,5 +1,5 @@
 # This Bucket is not the same as the state bucket
-# It will be created by terraform
+# It will be created by terraform after apply
 resource "aws_s3_bucket" "s3_bucket" {
   bucket = var.bucket_name
   acl    = "private"
